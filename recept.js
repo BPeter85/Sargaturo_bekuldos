937b9621b9d806e99da41efd939152be04883038
BPeter85 <peter.bognar85@gmail.com>
2023-04-01 09:57:33 +0200

    Bognár Péter - Sargaturo Beküldős feladat

---
+++ recept.js
@@ -0,0 +1,6 @@
+function hozzavalo(){
+    let ertek = document.getElementById('fore').value;
+    document.getElementById('tej').innerHTML = ertek /5;
+    document.getElementById('tojas').innerHTML = ertek * 2;
+    document.getElementById('cukor').innerHTML = ertek * 1;
+}
